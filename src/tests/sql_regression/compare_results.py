3bc2c4830f1fc986aa3228ee672f77fec1de15bc
Margrit Drescher <margrit2103@gmail.com>
2022-06-06 09:34:33 +0200

    private test margrit.yml

--- src/tests/sql_regression/compare_results.py
+++ src/tests/sql_regression/compare_results.py
@@ -2,11 +2,17 @@ import filecmp
 import glob
 import os
 
-d1 = "./goodx_repo/_dump/sql_regression/answ"
-d2 = "./goodx_repo/_dump/sql_regression/model_answ"
+result_path = "./goodx_repo/_dump/sql_regression"
+result_path = "c:/xxx"
 
-#d1 = "c:/xxx/answ"
-#d2 = "c:/xxx/model_answ"
+result_summary_file = f"{result_path}/test_result.txt"
+
+
+
+d1 = f"{result_path}/answ"
+d2 = f"{result_path}/model_answ"
+
+
 
 files=[]
 for fileName_relative in glob.glob(d2+"**/*.txt",recursive=True):       
@@ -14,15 +20,36 @@ for fileName_relative in glob.glob(d2+"**/*.txt",recursive=True):
 
 # shallow comparison
 match, mismatch, errors = filecmp.cmpfiles(d1, d2, files)
-print('Shallow comparison')
-print(f"File compared: {files}")
-print("Matched results:", match)
-print("Mismatched results:", mismatch)
-print("Errors:", errors)
-if len(errors) > 0:
-    raise Exception("**** Files not found in either the model or answ folders") 
-if len(mismatch) > 0:
-    raise Exception("**** Test answer does not correspond to model answer") 
+print(f"{result_path}/test_result.txt")
+with open(result_summary_file, 'w') as f:
+    #try:
+        f.write('readme')
+        f.write('Shallow comparison')
+        f.write(f"File compared: {files}")
+        f.write(f"Matched results: {match}")
+        f.write(f"Mismatched results: {mismatch}")
+        
+        f.write(f"Errors: {errors}")
+
+        _result = ''
+        if len(errors) > 0:
+            _result  = f"{_result} \n**** Files not found in either the model or answ folders"
+        if len(mismatch) > 0:
+            _result  = f"{_result} \n**** Test answer does not correspond to model answer"
+        if _result != '':    
+            f.write(_result)            
+        else:
+            f.write('OK')    
+    #except:
+    #    raise Exception(_result) 
+    #finally:        
+        f.close()
+        with open(result_summary_file, 'r') as f:
+            file_contents = f.read()
+            f.close()
+            print(file_contents)
+        
+        
 
 # deep comparison
 #match, mismatch, errors = filecmp.cmpfiles(d1, d2, files, shallow=False)
